4079d47ed1560ed47f577258f4fb8318b542eec2
cobalthex <mhines@betterworks.com>
2016-03-22 19:41:41 -0700

    Working on vm

--- ScriptingLang/Construct.hpp
+++ ScriptingLang/Construct.hpp
@@ -2,20 +2,28 @@
 
 #include "pch.hpp"
 #include "Reference.hpp"
-#include "Scope.hpp"
 
 namespace Plang
 {
+	class Scope;
+
+	using Primative = variant<void*, std::nullptr_t, bool, Int, Float, std::string>; //holds directly any primatives, or a pointer to complex data (arrays, etc)
+
+
+	//The basic object. Everything is a construct from literals to lexical blocks to functions
 	class Construct
 	{
 	public:
 		Construct() { }
+		~Construct() { }
 
 		bool IsCallable() const;
-		Plang::Reference Call(const Plang::Reference& Arguments, Scope* DotScope = nullptr, Scope* LexicalScope = nullptr);
+		Plang::Reference Call(const Plang::Reference& Arguments, Scope* DotScope = nullptr, Scope* LexScope = nullptr); //Passing Specific scopes predefines the scopes. No automatic inheritance is applied with custom scopes
 
 	protected:
-		Scope* lexicalScope;
-		Scope* dotScope;
+		Scope* lexScope; //the local (lexical) scope. Inherits from the lexical scope of the enclosing block. Created on call (or instantiation)
+		Scope* dotScope; //the public property scope. Inherits from accessor that calls this. Created on call (or instantiation)
+
+		Primative value;
 	};
 };
